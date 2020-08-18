71a01c4d74f8df3cc23812c0a9721bb98d108232
David Gan <dwgan@dwgan-mac.lan>
2020-08-18 10:09:37 -0700

    power_set and permutations

--- permutations.py
+++ permutations.py
@@ -2,11 +2,34 @@ from typing import List
 
 from test_framework import generic_test, test_utils
 
+import copy 
 
-def permutations(A: List[int]) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+# def permutations(A: List[int]) -> List[List[int]]:
 
+def permutations(A):
+    out = []
+    helper(A, 0, out)
+    return out
+
+def helper(A, curIdx, out):
+    if curIdx == len(A)-1:
+        #print(A)
+        out.append(copy.deepcopy(A))
+
+    # -- Each number should be the first element in the list
+    for j in range(curIdx, len(A)):
+        #print("curIdx is: ", curIdx, "j is: ", j)
+        A[curIdx], A[j] = A[j], A[curIdx]
+        #print("initial switch: ", A)
+        #  -- Generate all permutations for A[i+1]
+        helper(A, curIdx+1, out)
+        A[j], A[curIdx] = A[curIdx], A[j]
+        #print("switch back: ", A)
+    
+"""
+Time  : O(2^n) -- number of permutations of size n?
+Space : O()
+"""
 
 if __name__ == '__main__':
     exit(
--- power_set.py
+++ power_set.py
@@ -3,9 +3,31 @@ from typing import List
 from test_framework import generic_test, test_utils
 
 
-def generate_power_set(input_set: List[int]) -> List[List[int]]:
+# def generate_power_set(input_set: List[int]) -> List[List[int]]:
+#     # TODO - you fill in here.
+
+
+def generate_power_set(input_set):
     # TODO - you fill in here.
-    return []
+    out = [[]]
+    return find_powerset_of_ith_index(input_set, len(input_set)-1, out)
+
+
+def find_powerset_of_ith_index(input_set, i, out):
+    if i == -1:
+        return out
+
+    # P(x) = P(x-1) and then adding input_set at index x to all the previous sets
+    out = find_powerset_of_ith_index(input_set, i-1, out)
+    #print("out: ", out)
+    # Add the number from the ith index to all the previous subsets
+    out += [prevSet + [input_set[i]] for prevSet in out]
+    return out
+
+"""
+Time  : Each element can be in or not in a set of the powerset. so O(2^n)
+Space : Each element can be in or not in a set of the powerset. so O(2^n)
+"""
 
 
 if __name__ == '__main__':
